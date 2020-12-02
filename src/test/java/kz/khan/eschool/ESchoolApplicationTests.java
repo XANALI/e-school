991f09881232aad67a6028fe0d5383ffe5d15c5e
XANALI <53555116+XANALI@users.noreply.github.com>
2020-12-02 14:54:40 +0600

    Spring boot app initialization

---
+++ src/test/java/kz/khan/eschool/ESchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.khan.eschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
